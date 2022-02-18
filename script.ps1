75ac1eaf36f3476d4f66b5d6ab140efe5a786fb9
Administrateur <Administrateur@LY1217>
2022-02-18 19:55:08 +0100

    adding a script to install every plugins

---
+++ script.ps1
@@ -0,0 +1,5 @@
+bash -c 'npm init -y'
+bash -c 'yarn add --dev hardhat'
+bash -c 'yarn add --dev "hardhat@^2.8.4" "@nomiclabs/hardhat-waffle@^2.0.0" "ethereum-waffle@^3.0.0" "chai@^4.2.0" "@nomiclabs/hardhat-ethers@^2.0.0" "ethers@^5.0.0"'
+bash -c "yarn add --dev ts-node typescript"
+bash -c "yarn add --dev chai @types/node @types/mocha @types/chai"
